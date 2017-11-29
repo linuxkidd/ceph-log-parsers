8e859b0c55f9ee3f4c391a67096dd392f39ab49b
Michael J. Kidd <linuxkidd@users.noreply.github.com>
2017-11-29 11:19:39 -0800

    Update map_reporters_to_buckets.sh
    Remove non-allowed characters from bash variable name

--- map_reporters_to_buckets.sh
+++ map_reporters_to_buckets.sh
@@ -91,7 +91,8 @@ while read line; do
 				fi
 			}
 		fi
-		declare "${thirdcol}=${forthcol}"
+		thirdcolb=$(echo $thirdcol | sed -e 's/[^0-9a-zA-Z_]/_/g')
+		declare "${thirdcolb}=${forthcol}"
 	fi
 done < $2
 highest_bucket=${#buckets[*]}
