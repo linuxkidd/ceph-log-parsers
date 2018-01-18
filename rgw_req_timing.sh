84ba14e892d4d36a77276f039ddcee3a1e1a5e19
Michael J. Kidd <linuxkidd@linuxkidd.com>
2018-01-18 12:58:20 -0800

    Changed processing a bit to reduce duplicate code.

--- rgw_req_timing.sh
+++ rgw_req_timing.sh
@@ -11,45 +11,44 @@ if [ ! -e $1 ]; then
 	echo "File $1 does not exist."
 fi
 
-awk 'BEGIN {
-   mymin=999
-   mymax=0
-   mysum=0
-   mycount=0
+awk '
+  BEGIN {
+    mymin=999
+    mymax=0
+    mysum=0
+    mycount=0
   }
-  /starting new request/ {
-   MYREQ=$9
-   MYLINE=$0
-   gsub(/[-:]/," ",$1)
-   gsub(/[-:]/," ",$2)
-   MYSTART[MYREQ]=mktime($1" "$2)
-   split($2,secs,".")
-   millisecs=sprintf("0.%s",secs[2])
-   MYSTART[MYREQ]+=millisecs
+  /req=/ {
+    MYLINE=$0
+    gsub(/[-:]/," ",$1)
+    gsub(/[-:]/," ",$2)
+    MYTIME=mktime($1" "$2)
+    split($2,secs,".")
+    millisecs=sprintf("0.%s",secs[2])
+    MYTIME+=millisecs
+    if(match(MYLINE,/starting new request/)) {
+      MYREQ=$9
+      MYSTART[MYREQ]=MYTIME
+    }
+    if(match(MYLINE,/req done/)) {
+      MYREQ=$8
+      if(MYSTART[MYREQ]!="") {
+        mydiff=MYTIME-MYSTART[MYREQ]
+        if(mydiff<mymin) {
+          myminreq=MYLINE
+          mymin=mydiff
+        }
+        if(mydiff>mymax) {
+          mymaxreq=MYLINE
+          mymax=mydiff
+        }
+        mysum+=mydiff
+        mycount++
+        printf("%s,%s\n", mydiff, MYLINE)
+      }
+    }
   }
-  /req done/ {
-   MYLINE=$0;
-   if(MYSTART[$8]!="") {
-     gsub(/[-:]/," ",$1)
-     gsub(/[-:]/," ",$2)
-     MYSTOP=mktime($1" "$2)
-     split($2,secs,".")
-     millisecs=sprintf("0.%s",secs[2])
-     MYSTOP+=millisecs
-     mydiff=MYSTOP-MYSTART[$8]
-     if(mydiff<mymin) {
-       myminreq=MYLINE
-       mymin=mydiff
-     }
-     if(mydiff>mymax) {
-       mymaxreq=MYLINE
-       mymax=mydiff
-     }
-     mysum+=mydiff
-     mycount++
-     printf("%s,%s\n", mydiff, MYLINE)
-   }
- }
- END {
-   printf("Min,Avg,Max\n%s,%s,%s\nMin Req: %s\nMax Req: %s\n",mymin,mysum/mycount,mymax,myminreq,mymaxreq)
- }' $1
+  END {
+    printf("Min,Avg,Max\n%s,%s,%s\nMin Req: %s\nMax Req: %s\n",mymin,mysum/mycount,mymax,myminreq,mymaxreq)
+  }
+' $1
