987e4c3b0164572e4373eadad227f7c0dde61d35
Michael J. Kidd <linuxkidd@linuxkidd.com>
2018-01-18 10:02:01 -0800

    Parse ceph.log to providing time spent per deep-scrub

---
+++ deep-scrub_timing.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+awk 'BEGIN {
+   mymin=999
+   mymax=0
+   mysum=0
+   mycount=0
+  }
+  /deep-scrub/ {
+   MYLINE=$0
+   MYPG=$9
+   gsub(/[-:]/," ",$1)
+   gsub(/[-:]/," ",$2)
+   MYTIME=mktime($1" "$2)
+   split($2,secs,".")
+   millisecs=sprintf("0.%s",secs[2])
+   MYTIME+=millisecs
+
+   if($NF=="starts") {
+     MYSTART[MYPG]+=MYTIME
+     next
+   }
+    
+   if(MYSTART[MYPG]!="") {
+     mydiff=MYTIME-MYSTART[MYPG]
+     if(mydiff<mymin) {
+       myminreq=MYLINE
+       mymin=mydiff
+     }
+     if(mydiff>mymax) {
+       mymaxreq=MYLINE
+       mymax=mydiff
+     }
+     mysum+=mydiff
+     mycount++
+     printf("%s,%s\n", mydiff, MYLINE)
+   }
+ }
+ END {
+   printf("Min,Avg,Max\n%s,%s,%s\nMin Req: %s\nMax Req: %s\n",mymin,mysum/mycount,mymax,myminreq,mymaxreq)
+ }' $1
